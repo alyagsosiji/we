d46906e44d19b0cd230557cf4a4071196124be1c
알약소시지 <alyagsosiji@gmail.com>
2026-05-19 16:49:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1745,7 +1745,7 @@
         endingBadge: "memorySiteEndingCompleteBadge"
     };
 
-    const SITE_UPDATE_TEXT = "마지막 업데이트 2026.05.19 15:25";
+    const SITE_UPDATE_TEXT = "마지막 업데이트 : 2026.05.19 16:50";
 
     function safeGet(key) {
         try {
